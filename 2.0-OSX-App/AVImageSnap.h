3f6fa2739e1f5b587395ab17214f62c5e4ac63e2
Stan James <stan@wanderingstan.com>
2014-12-08 12:52:10 -0700

    moved artwork around, cleared out unused.

---
+++ 2.0-OSX-App/AVImageSnap.h
@@ -0,0 +1,13 @@
+//
+//  AVImageSnap.h
+//  LifeSlice
+//
+//  Created by Stan James on 12/8/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AVImageSnap : UITabBarController
+
+@end
---
+++ 2.0-OSX-App/AVImageSnap.m
@@ -0,0 +1,13 @@
+//
+//  AVImageSnap.m
+//  LifeSlice
+//
+//  Created by Stan James on 12/8/14.
+//
+//
+
+#import "AVImageSnap.h"
+
+@implementation AVImageSnap
+
+@end
